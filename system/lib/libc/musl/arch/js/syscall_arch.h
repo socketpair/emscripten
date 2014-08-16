c627a78142c1120bd05694905b06279644e3077d
Коренберг Марк (imac) <socketpair@gmail.com>
2014-08-17 01:49:19 +0600

    Introduce generic syscall interface
    This allows us to write syscall implementation in JS.
    
    This mean, for example, that we can compile full libc code, and
    implement only syscalls.
    
    This prevent us from reinventing wheel over and over while implementing
    some "new" libc function.
    
    When we implementing, say, getpid() function, we really implement
    getpid() syscall. But some logic in libc in that case is thrown out.
    This is wrong. We should not either throw or re-implement this logic.
    
    stay tuned.

--- system/lib/libc/musl/arch/js/syscall_arch.h
+++ system/lib/libc/musl/arch/js/syscall_arch.h
@@ -1,2 +1,41 @@
+#include <emscripten.h>
 
+#define __SYSCALL_LL_E(x) \
+((union { long long ll; long l[2]; }){ .ll = x }).l[0], \
+((union { long long ll; long l[2]; }){ .ll = x }).l[1]
+#define __SYSCALL_LL_O(x) 0, __SYSCALL_LL_E((x))
 
+static __inline long __syscall0(long n)
+{
+    return EM_ASM_INT({ syscall0($0); }, n);
+}
+
+static __inline long __syscall1(long n, long a1)
+{
+    return EM_ASM_INT({ syscall0($0, $1); }, n, a1);
+}
+
+static __inline long __syscall2(long n, long a1, long a2)
+{
+    return EM_ASM_INT({ syscall0($0, $1, $2); }, n, a1, a2);
+}
+
+static __inline long __syscall3(long n, long a1, long a2, long a3)
+{
+    return EM_ASM_INT({ syscall0($0, $1, $2, $3); }, n, a1, a2, a3);
+}
+
+static __inline long __syscall4(long n, long a1, long a2, long a3, long a4)
+{
+    return EM_ASM_INT({ syscall0($0, $1, $2, $3, $4); }, n, a1, a2, a3, a4);
+}
+
+static __inline long __syscall5(long n, long a1, long a2, long a3, long a4, long a5)
+{
+    return EM_ASM_INT({ syscall0($0, $1, $2, $3, $4, $5); }, n, a1, a2, a3, a4, a5);
+}
+
+static __inline long __syscall6(long n, long a1, long a2, long a3, long a4, long a5, long a6)
+{
+    return EM_ASM_INT({ syscall0($0, $1, $2, $3, $4, $5, $6); }, n, a1, a2, a3, a4, a5, a6);
+}
